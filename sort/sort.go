623d0942977fc0b6e3a1d59572309501ef19d15c
raphael2053 <raphael.guan@icloud.com>
2024-05-05 06:48:38 -0400

    quicksort

--- sort/sort.go
+++ sort/sort.go
@@ -9,3 +9,44 @@ func BubbleSort(nums []int) {
 		}
 	}
 }
+
+func QuickSort(nums []int) {
+	quickSort(nums, 0, len(nums)-1)
+}
+
+func quickSort(nums []int, left int, right int) {
+	// if the indexes are out of order, then there's nothing to do.
+	if left >= right {
+		return
+	}
+
+	// partition these elements around the pivot, then sort each side.
+	pivot := nums[(left+right)/2]
+	index := partition(nums, left, right, pivot)
+	quickSort(nums, left, index-1)
+	quickSort(nums, index, right)
+}
+
+func partition(nums []int, left int, right int, pivot int) int {
+	for left <= right {
+		// move the left pointer until element that is bigger than the pivot, because that should be on the right side.
+		for nums[left] < pivot {
+			left++
+		}
+
+		// move the left pointer until element that is smaller than the pivot, because that should be on the left side.
+		for nums[right] > pivot {
+			right--
+		}
+
+		// swap these values
+		if left <= right {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+			right--
+		}
+	}
+
+	// return the partition point which will be at the left point.
+	return left
+}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -29,3 +29,27 @@ func TestBubbleSort(t *testing.T) {
 		})
 	}
 }
+func TestQuickSort(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want []int
+	}{
+		{"empty", []int{}, []int{}},
+		{"one element", []int{1}, []int{1}},
+		{"two elements, sorted", []int{1, 2}, []int{1, 2}},
+		{"two elements, unsorted", []int{2, 1}, []int{1, 2}},
+		{"multiple elements, sorted", []int{1, 2, 3, 4}, []int{1, 2, 3, 4}},
+		{"multiple elements, unsorted", []int{4, 2, 3, 1}, []int{1, 2, 3, 4}},
+		{"all elements the same", []int{2, 2, 2, 2}, []int{2, 2, 2, 2}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			QuickSort(tt.nums)
+			if !reflect.DeepEqual(tt.nums, tt.want) {
+				t.Errorf("BubbleSort() = %v, want %v", tt.nums, tt.want)
+			}
+		})
+	}
+}
