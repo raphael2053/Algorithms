f978bca2e8990d903512e09d74b893160d28e19b
raphael.guan@icloud.com <raphael.guan@icloud.com>
2024-09-23 23:06:31 +0800

    add OpenFile function

--- tool/tool.go
+++ tool/tool.go
@@ -7,3 +7,21 @@ func TernaryOperator(condition bool, trueVal, falseVal interface{}) interface{} 
 	}
 	return falseVal
 }
+
+/* Opening a file */
+func OpenFile(fileName string) error {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	if f != nil {
+		defer func(f io.Closer) {
+			if err = f.Close(); err != nil {
+				fmt.Printf("defer close file %v %v\n", fileName, err)
+			}
+		}(f) // By taking advantaage of defer's property of evaluating first and then delaying execution, the file to be closed is specified at the time of registration,
+		// and it will be correctly closed when the function ends. This method is useful when mutiple files are opened.
+	}
+
+	return nil
+}
