3968ad0e5235a0481bd4e23241195d6122c808b0
raphael.guan@icloud.com <raphael.guan@icloud.com>
2024-09-26 23:38:18 +0800

    add reflection

--- tool/tool.go
+++ tool/tool.go
@@ -70,3 +70,57 @@ func comsumeTime() {
 
 	/* business logic below*/
 }
+
+// 通过反射机制来获取interface的字段和方法
+// func main() {
+
+// 	user := User{1, "Allen.Wu", 25}
+
+// 	DoFiledAndMethod(user)
+
+// }
+type User struct {
+	Id   int
+	Name string
+	Age  int
+}
+
+func (u User) ReflectCallFunc() {
+	fmt.Println("Allen.Wu ReflectCallFunc")
+}
+func (u User) ReflectCallFuncHasArgs(name string, age int) {
+	fmt.Println("ReflectCallFuncHasArgs name: ", name, ", age:", age, "and origal User.Name:", u.Name)
+}
+
+func (u User) ReflectCallFuncNoArgs() {
+	fmt.Println("ReflectCallFuncNoArgs")
+}
+func DoFiledAndMethod(input interface{}) {
+
+	getType := reflect.TypeOf(input)
+	fmt.Println("get Type is :", getType.Name())
+
+	getValue := reflect.ValueOf(input)
+	fmt.Println("get all Fields is:", getValue)
+
+	// 获取方法字段
+	// 1. 先获取interface的reflect.Type，然后通过NumField进行遍历
+	// 2. 再通过reflect.Type的Field获取其Field
+	// 3. 最后通过Field的Interface()得到对应的value
+	for i := 0; i < getType.NumField(); i++ {
+		field := getType.Field(i)
+		value := getValue.Field(i).Interface()
+		fmt.Printf("%s: %v = %v\n", field.Name, field.Type, value)
+	}
+
+	// 获取方法
+	// 1. 先获取interface的reflect.Type，然后通过.NumMethod进行遍历
+	// 2. 再分别通过reflect.Type的Method获取对应的真实的方法（函数）
+	// 3. 最后对结果取其Name和Type得知具体的方法名
+	// 4. 也就是说反射可以将“反射类型对象”再重新转换为“接口类型变量”
+	// 5. struct 或者 struct 的嵌套都是一样的判断处理方式
+	for i := 0; i < getType.NumMethod(); i++ {
+		m := getType.Method(i)
+		fmt.Printf("%s: %v\n", m.Name, m.Type)
+	}
+}
