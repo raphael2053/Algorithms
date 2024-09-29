2bc69f182a94edddd76f6e830129f6a9c54e76f0
raphael.guan@icloud.com <raphael.guan@icloud.com>
2024-09-29 23:38:18 +0800

    feat. add modifyInterface

--- tool/tool.go
+++ tool/tool.go
@@ -124,3 +124,25 @@ func DoFiledAndMethod(input interface{}) {
 		fmt.Printf("%s: %v\n", m.Name, m.Type)
 	}
 }
+
+// 通过反射机制修改interface的字段
+func modifyInterface() {
+	var num float64 = 1.2345
+	fmt.Println("old value of pointer:", num)
+
+	// 通过reflect.ValueOf获取num中的reflect.Value，注意，参数必须是指针才能修改其值
+	pointer := reflect.ValueOf(&num)
+	newValue := pointer.Elem()
+
+	fmt.Println("type of pointer:", newValue.Type())
+	fmt.Println("settability of pointer:", newValue.CanSet())
+
+	// 重新赋值
+	newValue.SetFloat(77)
+	fmt.Println("new value of pointer:", num)
+
+	////////////////////
+	// 如果reflect.ValueOf的参数不是指针，会如何？
+	pointer = reflect.ValueOf(num)
+	//newValue = pointer.Elem() // 如果非指针，这里直接panic，“panic: reflect: call of reflect.Value.Elem on float64 Value”
+}
